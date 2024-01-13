2378c3fe835bba3db7df916f82bbb5d7ad3b177a
Onesimeav <avallaonesime@gmail.com>
2024-01-13 07:10:03 +0100

    feat: Stripe credit card payment

--- android/app/src/main/kotlin/io/vhire/vhireapp/MainActivity.kt
+++ android/app/src/main/kotlin/io/vhire/vhireapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.vhire.vhireapp
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:provider/provider.dart';
 import 'package:vhireapp/shared/firebase_options.dart';
 import 'package:vhireapp/models/user.dart';
 import 'package:vhireapp/screens/wrapper.dart';
 import 'package:vhireapp/services/authentication.dart';
+import '.env';
+
+
+import 'bloc/payment/payment_bloc.dart';
 
 Future <void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey =publishablekey;
+  await Stripe.instance.applySettings();
   runApp(const MyApp());
 }
 
@@ -20,14 +29,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-  return StreamProvider<AuthenticatedUser?>.value(
-    initialData: null,
-    value: AuthService().user,
-    child: const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
-    ),
-  );
+    return StreamProvider<AuthenticatedUser?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => PaymentBloc(),
+          ),
+        ],
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Wrapper(),
+        ),
+      ),
+    );
   }
 }
+
 
